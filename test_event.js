3e7676aec438da44f187f9975529ecc32ae42fd1
analytics-scripts <54020175+analytics-scripts@users.noreply.github.com>
2019-08-12 12:21:41 +0300

    Update test_event.js

--- test_event.js
+++ test_event.js
@@ -1,6 +1,5 @@
 dataLayer.push({
-    'event': 'ee_event',
-    'event_category': 'github_test',
-    'event_action': 'test_action',
-    'event_non_interaction': true
+    'event': window.eventName,
+    'eventCategory': 'github_test',
+    'eventAction': 'test_action'
 });
